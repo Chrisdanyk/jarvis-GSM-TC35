2e8ea21e229aa97fc8890ffc26a7bdb0c6d859e6
Quentin Comte-Gaz <quentin@comte-gaz.com>
2017-04-18 11:52:24 +0200

    plugin: Add french command (+add config)

--- config.sh
+++ config.sh
@@ -1,2 +1,6 @@
 var_jv_pg_gt_com_port="COM4" # COM/USB port to communicate with the GSM module
 var_jv_pg_gt_pin=1234 # PIN number to connect to the SIM card (specify any number if no pin needed)
+
+# List here the people to potentially contact (used in command file)
+var_jv_pg_gt_jane_doe_phone="+33600000000"
+var_jv_pg_gt_john_doe_phone="+33600000000"
