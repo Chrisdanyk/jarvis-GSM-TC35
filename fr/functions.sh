b981c84dd66b80f9f8630b8f407a9a618ec8d0ba
Quentin Comte-Gaz <quentin@comte-gaz.com>
2017-04-18 13:21:13 +0200

    plugin fr: Delete unused variable

--- fr/functions.sh
+++ fr/functions.sh
@@ -5,9 +5,9 @@ jv_pg_gt_lang()
   case "$1" in
     send_sms_success) echo "SMS envoyé à $2.";;
     send_sms_failed) echo "Echec de l'envoi du SMS à $2.";;
-    call_success) zone_name=""; echo "Appel de $2 en cours.";;
-    call_failed) zone_name=""; echo "Echec de l'appel de $2.";;
-    hang_up_success) zone_name=""; echo "Arret de l'appel...";;
+    call_success) echo "Appel de $2 en cours.";;
+    call_failed) echo "Echec de l'appel de $2.";;
+    hang_up_success) echo "Arret de l'appel...";;
     hang_up_failed) echo "Echec de l'arret de l'appel.";;
     pick_up_success) echo "Appel en cours.";;
     pick_up_failed) echo "Impossible de décrocher l'appel.";;
