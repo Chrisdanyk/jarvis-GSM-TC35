febdfa435aed80577e3df6f03699797b91f15fc5
Quentin Comte-Gaz <quentin@comte-gaz.com>
2017-04-18 13:10:43 +0200

    install: Auto install pip and pyserial (used by the GSMTC35 lib)

--- install.sh
+++ install.sh
@@ -5,5 +5,11 @@
 [[ -z $(which python2.7) ]] && sudo apt-get install python2.7
 [[ -z $(which python3) ]] && sudo apt-get install python3
 
+# Install pip
+[[ -z $(which python-pip) ]] && sudo apt-get install python-pip
+
+# Install pyserial (needed for the GSMTC35 python library)
+sudo pip install pyserial
+
 # Copy latest version of GSM-TC35 python script
 wget -P plugins/jarvis-GSM-TC35/GSMTC35/GSMTC35.py https://raw.githubusercontent.com/QuentinCG/GSM-TC35-Python-Library/master/GSMTC35/GSMTC35.py
