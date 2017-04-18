b6aaa0fa592fba30ae105fdac2d6109e6e285874
Quentin Comte-Gaz <quentin@comte-gaz.com>
2017-04-18 11:51:50 +0200

    plugin: Add call/sms functions

--- functions.sh
+++ functions.sh
@@ -1,8 +1,62 @@
-#!/bin/bash
-# Here you can create functions which will be available from the commands file
-# You can also use here user variables defined in your config file
-# To avoid conflicts, name your function like this
-# pg_XX_myfunction () { }
-# pg for PluGin
-# XX is a short code for your plugin, ex: ww for Weather Wunderground
-# You can use translations provided in the language folders functions.sh
+#!/usr/bin/env bash
+
+# Send SMS
+# $1 (string): Phone number
+# $2 (string): SMS to send
+# $3 (string): Name of the person to send the SMS
+jv_pg_gt_send_sms()
+{
+  # Send request to GSM module
+  result="$(python plugins/jarvis-GSM-TC35/GSMTC35/GSMTC35.py --serialPort $var_jv_pg_gt_com_port --pin $var_jv_pg_gt_pin --sendSMS $1 \"$2\")"
+
+  # Show the result to user
+  if [[ $result =~ "True" ]]; then
+    say "$(jv_pg_gt_lang send_sms_success $3)"
+  else
+    say "$(jv_pg_gt_lang send_sms_failed $3)"
+  fi
+}
+
+# Call
+# $1 (string): Phone number
+# $2 (string): Name of the person to call
+jv_pg_gt_call()
+{
+  # Send request to GSM module
+  result="$(python plugins/jarvis-GSM-TC35/GSMTC35/GSMTC35.py --serialPort $var_jv_pg_gt_com_port --pin $var_jv_pg_gt_pin --call $1)"
+
+  # Show the result to user
+  if [[ $result =~ "True" ]]; then
+    say "$(jv_pg_gt_lang call_success $2)"
+  else
+    say "$(jv_pg_gt_lang call_failed $2)"
+  fi
+}
+
+# Hang up call
+jv_pg_gt_hang_up()
+{
+  # Send request to GSM module
+  result="$(python plugins/jarvis-GSM-TC35/GSMTC35/GSMTC35.py --serialPort $var_jv_pg_gt_com_port --pin $var_jv_pg_gt_pin --hangUpCall)"
+
+  # Show the result to user
+  if [[ $result =~ "True" ]]; then
+    say "$(jv_pg_gt_lang hang_up_success)"
+  else
+    say "$(jv_pg_gt_lang hang_up_failed)"
+  fi
+}
+
+# Pick up call
+jv_pg_gt_hang_up()
+{
+  # Send request to GSM module
+  result="$(python plugins/jarvis-GSM-TC35/GSMTC35/GSMTC35.py --serialPort $var_jv_pg_gt_com_port --pin $var_jv_pg_gt_pin --pickUpCall)"
+
+  # Show the result to user
+  if [[ $result =~ "True" ]]; then
+    say "$(jv_pg_gt_lang pick_up_success)"
+  else
+    say "$(jv_pg_gt_lang pick_up_failed)"
+  fi
+}
